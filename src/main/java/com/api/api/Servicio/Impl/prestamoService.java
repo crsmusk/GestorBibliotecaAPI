8d4edb798f6b9de0757f31d7647fae44199b8d39
cristian mosquera trullo <cristiantrullo41@gmail.com>
2024-11-10 20:46:08 -0500

    se agregaron nuevas funcionalidades y se corrigieron errores

--- src/main/java/com/api/api/Servicio/Impl/prestamoService.java
+++ src/main/java/com/api/api/Servicio/Impl/prestamoService.java
@@ -1,22 +1,21 @@
 package com.api.api.Servicio.Impl;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.api.api.Excepciones.Exceptions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.api.api.Excepciones.Exceptions.libroException;
-import com.api.api.Excepciones.Exceptions.libroNoDisponibleException;
-import com.api.api.Excepciones.Exceptions.personaException;
-import com.api.api.Excepciones.Exceptions.prestamoException;
 import com.api.api.Model.DTO.prestamoDto;
 import com.api.api.Model.Entities.libro;
-import com.api.api.Model.Entities.persona;
+import com.api.api.Model.Entities.cliente;
 import com.api.api.Model.Entities.prestamo;
 import com.api.api.Repositorio.libroRepository;
-import com.api.api.Repositorio.personaRepository;
+import com.api.api.Repositorio.clienteRepository;
 import com.api.api.Repositorio.prestamoRepository;
 import com.api.api.Servicio.Interface.Iprestamo;
 import com.api.api.mapper.prestamoMapper;
@@ -28,7 +27,7 @@ public class prestamoService implements Iprestamo {
  @Autowired
  private libroRepository libroRepo;
  @Autowired 
- private personaRepository personaRepo;
+ private clienteRepository personaRepo;
  @Autowired
  private prestamoMapper mapper;
  
@@ -36,59 +35,128 @@ public class prestamoService implements Iprestamo {
  @Override
  @Transactional(readOnly = true)
 public List<prestamoDto> findAll() {
-  List<prestamoDto>lista=mapper.toprestamosDto(prestamoRepo.findAll());
-  return lista;
+  List<prestamo>prestamos=prestamoRepo.findAll();
+  if (prestamos.isEmpty()) {
+    throw new noHayContenidoException();
+  }else{
+    return mapper.toprestamosDto(prestamos);
+  }  
 }
 
 @Override
 @Transactional(readOnly = true)
-public Optional<prestamoDto> findById(Long id) {
-  prestamo presta=prestamoRepo.findById(id).orElseThrow(()-> new prestamoException("no se encontro el prestamo"));
-  prestamoDto prestamo=mapper.toPrestamoDto(presta);
-  return Optional.of(prestamo);
-
+public prestamoDto findById(Long id) {
+  Optional<prestamo>prestamo=prestamoRepo.findById(id);
+  if (prestamo.isPresent()) {
+  return mapper.toPrestamoDto(prestamo.get());
+  }else{
+    throw new prestamoException("no se encontro el prestamo con el id "+id);
+  }
 }
 
 @Override
 @Transactional
 public void save(prestamoDto prestamoDTO) {
-   persona persona=personaRepo.findByEmail(prestamoDTO.getPersonaDto().getEmail()).orElseThrow(()->new personaException("no se encontro al usuario"));
-   libro libro=libroRepo.findByTituloIgnoreCase(prestamoDTO.getLibroDto().getTitulo()).orElseThrow(()->new libroException("no se encontro el libro"));
-   if(libro.isEstado()==false){
-     throw new libroNoDisponibleException("libro no disponible");
-   }
-   libro.setEstado(false);
    prestamo prestamo=new prestamo();
-   prestamo.setLibro(libro);
-   prestamo.setPersona(persona);
-   prestamo.setFechaDePrestamo(prestamoDTO.getFechaDePrestamo());
-   prestamo.setFechaDeEntrega(prestamoDTO.getFechaDeEntrega());
-   prestamoRepo.save(prestamo);
-   
+   Optional<cliente>cliente=personaRepo.findByEmail(prestamoDTO.getEmailCliente());
+   List<libro>libros=new ArrayList<>();
+   for (String i:prestamoDTO.getNombreLibro()){
+       if (libroRepo.findByTituloIgnoreCase(i).isPresent()){
+           if (libroRepo.findByTituloIgnoreCase(i).get().isEstado()){
+               libros.add(libroRepo.findByTituloIgnoreCase(i).get());
+           }else{
+               throw new libroNoDisponibleException("el libro "+i+" no esta disponible");
+           }
+       }else{
+           throw new prestamoException("no se encontro el libro con el titulo"+i);
+       }
+   }
+
+   if (cliente.isPresent()) {
+    prestamo.setLibros(libros);
+    prestamo.setCliente(cliente.get());
+    prestamo.setFechaDePrestamo(prestamoDTO.getFechaDePrestamo());
+    prestamo.setFechaDeEntrega(prestamoDTO.getFechaDeEntrega());
+    prestamo.setEstadoPrestamo(true);
+    prestamoRepo.save(prestamo);
+   }else{
+     throw new personaException("no se encontro la persona con el email "+prestamoDTO.getEmailCliente());
+   }
 }
 
 @Override
 @Transactional
 public void delete(Long id) {
-  prestamoRepo.deleteById(id);
+  if (personaRepo.existsById(id)) {
+    prestamoRepo.deleteById(id);
+  }else{
+    throw new prestamoException("no se encontro el prestamo con el id "+id);
+  }
+  
 }
 
 @Override
 @Transactional
-public void update(Long id, prestamoDto prestamoDTO) {
-   prestamo prestamo=prestamoRepo.findById(id).orElseThrow(()-> new prestamoException("no se econtro el prestamo con el id "+id));
-    persona persona=personaRepo.findByEmail(prestamoDTO.getPersonaDto().getEmail()).orElseThrow(()->new personaException("no se encontro al usuario con el correo"+prestamoDTO.getPersonaDto().getEmail()));
-    libro libro=libroRepo.findByTituloIgnoreCase(prestamoDTO.getLibroDto().getTitulo()).orElseThrow(()->new libroException("no se econtro el libro con el titulo"+prestamoDTO.getLibroDto().getTitulo()));
-    
+public void addBook(Long id, String titulo) {
+   Optional<prestamo>prestamo=prestamoRepo.findById(id);
+    if (!prestamo.isPresent()) {
+        throw new prestamoException("no se encontro el prestamo con el id"+id);
+    }
+    libro libro=libroRepo.findByTituloIgnoreCase(titulo).orElseThrow(()->new libroException("no se encontrro el libro"));
     libro.setEstado(false);
-    prestamo.setLibro(libro);
-    prestamo.setPersona(persona);
-    prestamo.setFechaDePrestamo(prestamoDTO.getFechaDePrestamo());
-    prestamo.setFechaDeEntrega(prestamoDTO.getFechaDeEntrega());
-    prestamoRepo.save(prestamo);
+    libroRepo.save(libro);
+    prestamo.get().setLibros(List.of(libro));
+    prestamoRepo.save(prestamo.get());
+
 }
 
+    @Override
+    @Transactional
+    public void returnBook(Long id,String titulo) {
+        Optional<prestamo>prestamo=prestamoRepo.findById(id);
+        if (prestamo.isPresent()){
+            if (prestamo.get().getFechaDeEntrega().isBefore(LocalDate.now())){
+                throw new prestamoAtrasadoException("el prestamo esta atrasado");
+            }
+            libro libro=libroRepo.findByTituloIgnoreCase(titulo).orElseThrow(()->new libroException("libro no encontrado"));
+            libro.setEstado(true);
+            libroRepo.save(libro);
+            List<libro>libros=new ArrayList<>();
+            for (libro i:prestamo.get().getLibros()){
+                if (i.getTitulo().equalsIgnoreCase(libro.getTitulo())){
+                    continue;
+                }
+                libros.add(i);
+            }
+            prestamo.get().setLibros(libros);
+            if (prestamo.get().getLibros().isEmpty()){
+                prestamo.get().setEstadoPrestamo(false);
+            }
+            prestamoRepo.save(prestamo.get());
+        }else{
+            throw  new prestamoException("no se encontro el prestamo con el id "+id);
+        }
+    }
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<prestamoDto>lateLoans() {
+        List<prestamo>prestamos=prestamoRepo.findByFechaDeEntregaLessThan(LocalDate.now());
+        if (prestamos.isEmpty()){
+            throw new noHayContenidoException();
+        }
+        return mapper.toprestamosDto(prestamos);
+    }
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<prestamoDto> loansEarrings() {
+        List<prestamo>prestamos=prestamoRepo.findByEstadoPrestamoFalse();
+        if (prestamos.isEmpty()){
+            throw new noHayContenidoException();
+        }else{
+            return mapper.toprestamosDto(prestamos);
+        }
+    }
 
 }
