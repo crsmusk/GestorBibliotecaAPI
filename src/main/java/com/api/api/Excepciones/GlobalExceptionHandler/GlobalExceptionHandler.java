23785d21baabe6896623070cf850de5d4fe94a03
cristian mosquera trullo <cristiantrullo41@gmail.com>
2024-11-10 20:43:18 -0500

    se agregaron excepciones

--- src/main/java/com/api/api/Excepciones/GlobalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/api/api/Excepciones/GlobalExceptionHandler/GlobalExceptionHandler.java
@@ -1,17 +1,12 @@
 package com.api.api.Excepciones.GlobalExceptionHandler;
 
+import com.api.api.Excepciones.Exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.api.api.Excepciones.ExceptionClass.Error;
-import com.api.api.Excepciones.Exceptions.autorException;
-import com.api.api.Excepciones.Exceptions.generoException;
-import com.api.api.Excepciones.Exceptions.libroException;
-import com.api.api.Excepciones.Exceptions.libroNoDisponibleException;
-import com.api.api.Excepciones.Exceptions.personaException;
-import com.api.api.Excepciones.Exceptions.prestamoException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -39,11 +34,11 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(prestamoException.class)
   public ResponseEntity<Error>manejarPrestamoExcepcio(prestamoException ex){
     Error error=new Error(
-      HttpStatus.CONFLICT.value(),
+      HttpStatus.NOT_FOUND.value(),
       ex.getMessage(),
       System.currentTimeMillis()
     );
-    return new ResponseEntity<>(error,HttpStatus.CONFLICT);
+    return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
   }
 
   
@@ -76,4 +71,54 @@ public class GlobalExceptionHandler {
     );
     return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
   }
+
+  @ExceptionHandler(prestamoAtrasadoException.class)
+    public ResponseEntity<Error>manejarPrestamoAtrasadoException(prestamoAtrasadoException ex){
+      Error error=new Error(
+              HttpStatus.CONFLICT.value(),
+              ex.getMessage(),
+              System.currentTimeMillis()
+      );
+      return new ResponseEntity<>(error,HttpStatus.CONFLICT);
+  }
+
+    @ExceptionHandler(noHayContenidoException.class)
+    public ResponseEntity<Error>manejarNoHayContenidoException(noHayContenidoException ex){
+        Error error=new Error(
+                HttpStatus.NO_CONTENT.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error,HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(usuarioNoEncontradoException.class)
+    public ResponseEntity<Error>manejarUsuarioNoEncontradoException(usuarioNoEncontradoException ex){
+        Error error=new Error(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(rolNoEncontradoException.class)
+    public ResponseEntity<Error>manejarRolNoEncontradoException(rolNoEncontradoException ex){
+     Error error=new Error(
+             HttpStatus.NOT_FOUND.value(),
+             ex.getMessage(),
+             System.currentTimeMillis()
+     );
+     return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(permisoNoEncontradoException.class)
+    public ResponseEntity<Error>manejarPermisoNoEncontradoException(permisoNoEncontradoException ex){
+     Error error=new Error(
+             HttpStatus.NOT_FOUND.value(),
+             ex.getMessage(),
+             System.currentTimeMillis()
+     );
+     return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
 }
